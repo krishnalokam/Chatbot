57edfdf8403969038f9250f80ab304f8e7b35527
krishnalokam <siva.lokam4work@gmail.com>
2021-12-06 22:35:35 +0530

    initialize chatbot

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./components/Header";
+import Chat from "./components/Chat";
+
+
+function App() {
+    
+  return (
+    <div className="App">
+     <Header />
+     <Chat />
+    </div>
+  );
+}
+
+export default App;
